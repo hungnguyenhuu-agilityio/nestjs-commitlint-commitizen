ebccfa249aa5a54e8a0b07222775c0a5557c2fba
Hung Nguyen Huu <hung.nguyenhuu@asnet.com.vn>
2024-10-18 11:02:26 +0700

    chore(husky): update commit hook with test and format

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { Environment } from './config/environment.enum';
 
-
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
