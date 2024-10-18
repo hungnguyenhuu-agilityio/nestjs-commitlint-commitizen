4ccad496e8464ba92062e536c65751dc5b4e2fda
Hung Nguyen Huu <hung.nguyenhuu@asnet.com.vn>
2024-10-18 10:38:09 +0700

    chore(logger): adding

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, LogLevel } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { Environment } from './config/environment.enum';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const isProduction: boolean =configService.get<string>('NODE_ENV') === Environment.Production;
+  const logger: LogLevel[] = isProduction
+    ? ['debug', 'warn', 'error']
+    : ['warn', 'error'];
+  Logger.overrideLogger(logger);
+
+  await app.listen(configService.get<number>('PORT') || 3000);
 }
 bootstrap();
