d713b689258e4d785cb1395d59a3dd8a9a787451
Hung Nguyen Huu <hung.nguyenhuu@asnet.com.vn>
2024-10-21 17:22:25 +0700

    build(swagger): implementation with config

---
+++ src/core/constants/swagger.constant.ts
@@ -0,0 +1,13 @@
+/**
+ * Define constant for the config of Swagger
+ */
+export const SwaggerConfig = {
+  // Title for the API documentation
+  title: 'API Documentation',
+
+  // Some description to describe the purpose of application
+  description: 'Boilerplate description for NestJS',
+
+  // Giving the version for the application
+  version: '1.0'
+}
--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,29 @@ import { Logger, LogLevel } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { Environment } from './config/environment.enum';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerConfig } from './core/constants/swagger.constant';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  // Logger session
   const isProduction: boolean =
     configService.get<string>('NODE_ENV') === Environment.Production;
   const logger: LogLevel[] = isProduction
     ? ['debug', 'warn', 'error']
     : ['warn', 'error'];
   Logger.overrideLogger(logger);
+
+  // Swagger session
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(SwaggerConfig.title)
+    .setDescription(SwaggerConfig.description)
+    .setVersion(SwaggerConfig.version)
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, documentFactory);
 
   await app.listen(configService.get<number>('PORT') || 3000);
 }
